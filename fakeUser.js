239e0cef6ddeb0a65377d95f3e5e89e730c0be23
chris <chrisrhayden@gmail.com>
2017-10-26 05:05:20 -0700

    added comments

--- fakeUser.js
+++ fakeUser.js
@@ -1,16 +1,20 @@
-const faker = require('faker')
+// avatar-generator needs to be called ()
+const avGen = require('avatar-generator')()
 const bluIMd5 = require('blueimp-md5')
 const fetch = require('node-fetch')
-const avGen = require('avatar-generator')()
+const faker = require('faker')
 
+const myURL = 'https://fakeuser-92f5d.firebaseio.com/thing.json'
 let apiCallCounter = 0
 
+// for fake api
 const getGender = (rate) => {
-  if (Math.random < rate) return 'male'
+  if (Math.random() < rate) return 'male'
   else return 'female'
 }
 
 const fakeApi = async () => {
+  // <---------- faker calls
   const firstName = faker.name.firstName()
   const lastName = faker.name.lastName()
   const gender = getGender(0.5)
@@ -22,16 +26,19 @@ const fakeApi = async () => {
   const city = faker.address.city()
   const state = faker.address.state()
   const zip = faker.address.zipCode()
+  // ---------->
 
+  // promise a str of image bytes or whatever
   const getAvatar = new Promise((resolve, reject) => {
     avGen(firstName, gender, 400).toBuffer((err, buffer) => {
-      if (err) reject(new Error('cant get img, idk why'))
+      if (err) reject(new Error('cant get img, idk why', err))
+      // when the buffer is finished convert to str and return/resolve
       resolve(buffer.toString('base64'))
     })
   })
 
-  // await getAvatar, it will return a str so no need for conversion
-  let avatar = await getAvatar.then(resp => resp).catch(console.log)
+  // wait for the getAvatar promise to resolve then assign to avatar
+  const avatar = await getAvatar.then(resp => resp).catch(console.log)
   const myApiInfo = {
     apiCallCounter: apiCallCounter++,
     avatar,
@@ -49,33 +56,53 @@ const fakeApi = async () => {
   return myApiInfo
 }
 
-const callApi = async () => {
-  /* is it because fakeApi runs async or promise that we need to wait on
-   * or the init when fetch is called will be empty */
-  let myInit = {
+const apiFetchCall = async () => {
+  /* wait for fakeApi() call to resolve,
+   * it will return a promise due to getAvatar() I guess */
+  const myInit = {
     method: 'POST',
     body: JSON.stringify(await fakeApi())
   }
 
-  let response = await fetch(myURL, myInit)
-  let output = await response.json()
+  // push to server, accept server response and convert
+  const response = await fetch(myURL, myInit)
+  const output = await response.json()
 
-  return new Promise((resolve, reject) => {
-    resolve(output)
-    reject(new Error('idk what happend'))
-  })
+  return output
 }
 
-let myURL = 'https://fakeuser-92f5d.firebaseio.com/thing.json'
+const makeAllApiCalls = async () => {
+  // use async to block the fetch delete
+  await fetch(myURL, {method: 'DELETE'})
+  // make and array the fill with apiFetchCall function then map and call
+  const allApiCalls = Array(100).fill(apiFetchCall).map((call) => call())
 
+  /* if apiFetchCall returns a promise Promise.all will block
+   * else if it just returns output its nonblocking,
+   * eventually resolving and getting printed */
+  Promise.all(allApiCalls)
+    .then(console.log)
+    .catch(console.log)
+  console.log('api call counter >>', apiCallCounter)
+}
+
+makeAllApiCalls()
+console.log('api call counter >>', apiCallCounter)
+
+/*
+return new Promise((resolve, reject) => {
+  resolve(output)
+  reject(new Error('idk what happend'))
+})
 fetch(myURL, {method: 'DELETE'})
   .then(() => {
     // make and array with uncalled functions then map and call inside
     const allApiCalls = Array(100).fill(callApi).map((call) => call())
-    /* take an array of called functions giving promises
-     * and wait till all resolve */
+    take an array of called functions giving promises
+    and wait till all resolve
     Promise.all(allApiCalls)
       .then(console.log)
       .catch(console.log)
   }) // end of the fetch delete .then
   .catch(console.log) // for the delete fetch call
+*/
