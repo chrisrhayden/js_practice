38ba1cdac187de623308c1ceb17e55bd71ff6ae1
chris <chrisrhayden@gmail.com>
2017-11-16 17:41:46 -0800

    it fucking works

--- balanceThing.js
+++ balanceThing.js
@@ -3,31 +3,30 @@ const balancedBrackets = (theStr) => {
   const closeTags = [')', ']', '}']
 
   let openList = []
-  let closedList = []
+  let closList = []
+
   for (let i in theStr) {
     if (openTags.includes(theStr[i])) {
       openList.push(theStr[i])
     } else if (closeTags.includes(theStr[i])) {
-      const ind1 = closedList.indexOf(theStr[i])
-      const ind2 = openList.indexOf(theStr[i - 1])
-      if (ind1 === ind2) {
-        console.log(theStr[i], theStr[i - 1])
-        openList.pop()
-      } else {
-        closedList.push(theStr[i])
+      const ind1 = closeTags.indexOf(theStr[i])
+      const otag = openList.pop()
+      const ind2 = openTags.indexOf(otag)
+      if (ind1 !== ind2) {
+        return false
       }
     }
   }
-  if (openList) {
-    console.log('>>>', openList.join('') + closedList.join(''))
-    return balancedBrackets(openList.join('') + closedList.join(''))
-  } else if (!openList) {
-    return true
-  } else {
-    console.log('idk what to do, i guse its false')
-    return false
-  }
+  console.log(openList, closList)
+  return openList.length === 0
 }
 
-console.log(balancedBrackets('()[]{}(([])){[()][]}'))
-console.log(balancedBrackets('()[]{}(([]))[{()][]}'))
+console.log(balancedBrackets('()[]{}(([])){[()][]}')) // true)
+console.log(balancedBrackets('())[]{}')) // false
+console.log(balancedBrackets('[(])')) // false
+/*
+console.log(balancedBrackets('()[]{}(([])){[()][]}')) // true
+console.log(balancedBrackets('()[]{}(([]))[{()][]}')) // false
+console.log(balancedBrackets('())[]{}')) // false
+console.log(balancedBrackets('[(])')) // false
+*/
